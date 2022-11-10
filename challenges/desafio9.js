4659955dd94f5d3f69973a0480fa5423a197302b
Julia <juliamarques61@gmail.com>
2022-11-10 16:26:12 -0300

    feat: correção de lint

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,6 +1,6 @@
 // 9 - Retorne o nome de todos os lanches que possuam calorias abaixo de 500
 
 db.produtos.find(
-  { 'valoresNutricionais.0.quantidade': { $lt: 500 } },
+  { "valoresNutricionais.0.quantidade": { $lt: 500 } },
   { _id: 0, nome: 1 },
 );
