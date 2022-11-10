c06ccc86ddf73507fdfda625b8923e23557e944f
Julia <juliamarques61@gmail.com>
2022-11-10 17:11:49 -0300

    feat: retorna a quantidade de produtos do resumoProdutos

---
+++ challenges/desafio17.js
@@ -0,0 +1,10 @@
+// 17 - Retorne a quantidade total de produtos em uma nova coleção chamada resumoProdutos
+
+db.resumoProdutos.insertOne(
+  { franquia: "McDonalds", totalProdutos: db.produtos.countDocuments({})},
+);
+
+db.resumoProdutos.find(
+  {},
+  { _id: 0, franquia: 1, totalProdutos: 1 },
+);
