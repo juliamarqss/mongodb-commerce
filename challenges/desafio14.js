7fe4f6e8de7396f95ccb001fc954d1d8064cb8e4
Julia <juliamarques61@gmail.com>
2022-11-10 16:59:49 -0300

    feat: retorna todos os lanches que possuem picles

---
+++ challenges/desafio14.js
@@ -0,0 +1,6 @@
+// 14 - Crie uma query que retorne todos os lanches que possuem picles em seus ingredientes e mostre apenas os 3 primeiros itens contidos no array valoresNutricionais
+
+db.produtos.find(
+  { ingredientes: "picles" },
+  { _id: 0, nome: 1, valoresNutricionais: { $slice: 3 }, ingredientes: 1 }
+);
